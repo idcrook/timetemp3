9e8c21ff14a4bbae93d229543aa9ae5b491a627a
David Crook <idcrook@users.noreply.github.com>
2020-06-14 11:15:14 -0600

    port 7segment clock from earlier project

---
+++ timetemp3/__init__.py
@@ -0,0 +1,81 @@
+
+import time
+import datetime
+
+from Adafruit_LED_Backpack import SevenSegment
+
+# magic numbers for storing / passing time digits
+DIGIT_1 = 0
+DIGIT_2 = 1
+DIGIT_3 = 2
+DIGIT_4 = 3
+DIGIT_COLON = 4
+
+# Set to 12 or 24 hour mode
+HOUR_MODE_12_OR_24 = 12
+
+# I2C address of display
+LED_SEGMENT_I2C_ADDRESS = 0x70
+
+# Number of seconds to wait after display is written
+DISPLAY_SLEEP_DURATION = 1/4 
+
+def get_time_digits( now, hour_mode = HOUR_MODE_12_OR_24, toggle_colon = True):
+    digits = [None] * 5
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+
+    if hour_mode != 24:
+        if hour > 12:  # handle 13 through 23
+            hour = hour - 12
+        if hour == 0:  # handle 0 (hour of midnight)
+            hour = 12
+
+    # Set hours digits
+    if hour >= 10 or hour == 0:  # Tens
+        digits[DIGIT_1] = int(hour / 10)
+    else:
+        digits[DIGIT_1] = ' '
+
+    digits[DIGIT_2] = hour % 10 # Ones
+
+    # Set minutes digits
+    digits[DIGIT_3] = int(minute / 10)  # Tens
+    digits[DIGIT_4] = minute % 10  # Ones
+
+    # Toggle colon
+    if toggle_colon:
+        digits[DIGIT_COLON] = second % 2 # Toggle colon at 0.5 Hz (every other second)
+    else:
+        digits[DIGIT_COLON] = True
+
+    return digits
+
+
+def initialize_and_get_time_display_handle (i2c_address = LED_SEGMENT_I2C_ADDRESS):
+    segment = SevenSegment.SevenSegment(address=i2c_address)
+    # Initialize display. Must be called once before using the display.
+    segment.begin()
+    print("Using time display I2C address: 0x%02x" % (i2c_address,))
+    return segment
+
+def display_time_digits(time_digits, sleep_duration = DISPLAY_SLEEP_DURATION, display_handle = None):
+    segment = display_handle
+
+    if segment:
+        segment.clear()
+
+        segment.set_digit(DIGIT_1, time_digits[DIGIT_1])
+        segment.set_digit(DIGIT_2, time_digits[DIGIT_2])
+        segment.set_digit(DIGIT_3, time_digits[DIGIT_3])
+        segment.set_digit(DIGIT_4, time_digits[DIGIT_4])
+        segment.set_colon(time_digits[DIGIT_COLON] )
+
+        # Write the display buffer to the hardware.  This must be called to
+        # update the actual display LEDs.
+        segment.write_display()
+
+        # sleep_duration should be less than 1 second to prevent colon jittering
+        time.sleep(sleep_duration)
+
--- timetemp3/my_7segment_clock.py
+++ timetemp3/my_7segment_clock.py
@@ -1,5 +1,78 @@
-import timetemp3
+#!/usr/bin/env python3
 
+import time
+import datetime
+import os
+import signal
+from sys import exit
+
+from . import initialize_and_get_time_display_handle, get_time_digits, display_time_digits
+
+# To run: python3 ./my_7segment_clock.py
+
+# Set to 12 or 24 hour mode
+HOUR_MODE_12_OR_24 = 12
+
+# I2C address of display
+LED_SEGMENT_I2C_ADDRESS = 0x70
+# LED_SEGMENT_I2C_ADDRESS = 0x71
+
+# Number of seconds to wait after display is written
+DISPLAY_SLEEP_DURATION = 1/4
+
+# Initialize LED display
+segment = initialize_and_get_time_display_handle(i2c_address = LED_SEGMENT_I2C_ADDRESS)
+
+io_error_count = 0
+
+# via https://stackoverflow.com/questions/18499497/how-to-process-sigterm-signal-gracefully
+class GracefulKiller:
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        print(
+            "Received signal "
+            + str(signum)
+            + " on line "
+            + str(frame.f_lineno)
+            + " in "
+            + frame.f_code.co_filename
+        )
+        self.kill_now = True
 
 def main():
-    print(__name__, __file__, timetemp3.__doc__)
+
+    def graceful_exit():
+        # Turn off LED 
+        segment.clear()
+        segment.write_display()
+        exit(0)
+
+    # output current process id
+    print("My PID is:", os.getpid())
+    killer = GracefulKiller()
+
+    print("Starting main loop")
+    print("Press CTRL+C to exit")
+    while not killer.kill_now:
+        # Periodically update the time on a 4 char, 7-segment display
+        try:
+            now = datetime.datetime.now()
+            clock_digits = get_time_digits(now = now)
+            #print(clock_digits)
+            display_time_digits(clock_digits, sleep_duration = DISPLAY_SLEEP_DURATION, display_handle = segment)
+
+        except KeyboardInterrupt:
+            graceful_exit()
+
+        # IOError: [Errno 121] Remote I/O error would occasionally surface on Raspian stretch
+        except IOError:
+            io_error_count += 1
+            print("Caught ", io_error_count, "IOErrors")
+            time.sleep(2)
+
+    graceful_exit()
