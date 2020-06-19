a18a77df94bc7e3528af9ba63160afb01932a28f
David Crook <idcrook@users.noreply.github.com>
2020-06-18 20:37:39 -0600

    add service install script

--- README.md
+++ README.md
@@ -69,33 +69,8 @@ On my Raspberry Pi OS system, `groups` includes "`gpio i2c`" so these services d
 Install user unit file. Assumes git clone at `/home/pi/projects/timetemp3/` and has been installed
 
 ```shell
-# refer to https://www.freedesktop.org/software/systemd/man/systemd.unit.html#User%20Unit%20Search%20Path
-MY_SYSTEMD_USER_UNIT_DIR=/home/pi/.config/systemd/user/
-mkdir -p "${MY_SYSTEMD_USER_UNIT_DIR}"
 cd /home/pi/projects/timetemp3/
-cp -av etc/timetemp_7segment_clock.service "${MY_SYSTEMD_USER_UNIT_DIR}"
-cp -av etc/timetemp_weather_logging.service "${MY_SYSTEMD_USER_UNIT_DIR}"
-
-# activate
-systemctl --user list-unit-files | grep timetemp
-# next command needed even though it is listed in previous output
-systemctl --user daemon-reload
-
-systemctl --user start timetemp_7segment_clock
-systemctl --user status timetemp_7segment_clock
-journalctl --user-unit timetemp_7segment_clock.service
-
-systemctl --user start timetemp_weather_logging
-systemctl --user status timetemp_weather_logging
-journalctl --user-unit timetemp_weather_logging.service
-
-# Automatically start
-systemctl --user enable timetemp_7segment_clock
-systemctl --user enable timetemp_weather_logging
-
-# Run on system startup
-sudo loginctl enable-linger pi
-sudo loginctl user-status pi
+conf/install_services.sh
 ```
 
 TODO
---
+++ conf/install_services.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#!/bin/bash -x
+
+# wrapper script to install systemd user unit
+
+clone_dir=/home/pi/projects/timetemp3
+
+if [[ -d "${clone_dir}" ]] ; then
+    echo Using clone "${clone_dir}"
+else
+    echo Missing directory "${clone_dir}"
+    exit 1
+fi
+
+cd "${clone_dir}"/
+
+# refer to https://www.freedesktop.org/software/systemd/man/systemd.unit.html#User%20Unit%20Search%20Path
+MY_SYSTEMD_USER_UNIT_DIR=/home/pi/.config/systemd/user/
+mkdir -p "${MY_SYSTEMD_USER_UNIT_DIR}"
+cp -av etc/timetemp_7segment_clock.service  "${MY_SYSTEMD_USER_UNIT_DIR}"
+cp -av etc/timetemp_weather_logging.service "${MY_SYSTEMD_USER_UNIT_DIR}"
+
+systemctl  --user list-unit-files | grep timetemp
+# next command needed even though listed in previous output
+systemctl  --user daemon-reload
+
+systemctl  --user start  timetemp_7segment_clock
+systemctl  --user status timetemp_7segment_clock | cat
+echo " journalctl --user-unit   timetemp_7segment_clock.service"
+
+systemctl  --user start  timetemp_weather_logging
+systemctl  --user status timetemp_weather_logging | cat
+echo " journalctl --user-unit   timetemp_weather_logging.service"
+
+# Automatically start
+systemctl --user enable  timetemp_7segment_clock
+systemctl --user enable  timetemp_weather_logging
+
+echo ""
+echo "Run to allow user services to run when not logged on"
+echo ""
+echo " sudo loginctl enable-linger pi"
+echo " sudo loginctl user-status pi"
+echo ""
